e37871bbdb9df761e77d5bb6e5b0813ef3f9461b
yangshenglan <yangshenglan@58.com>
2020-08-27 12:22:29 +0800

    fix

--- src-web/src/services/index.js
+++ src-web/src/services/index.js
@@ -1,7 +1,7 @@
 import {request} from '../utils/request';
 import { stringify } from 'qs';
-// const host = "http://10.252.218.173:3001"
-const host = "http://127.0.0.1:3001"
+const host = "http://10.252.218.173:3001"
+// const host = "http://127.0.0.1:3001"
 //列表页接口
 export function getReceiveList(params) {
   return request(`${host}/receive/list?${stringify(params)}`);
